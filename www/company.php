806bd699386f1f4bd1df69a6f30fc086a749de1a
Кирилл Мардашов <kirillmardasov@iMac-Kirill.local>
2017-02-16 23:30:23 +0300

    Made Alik's commits

---
+++ www/company.php
@@ -0,0 +1,108 @@
+﻿<?include 'header.html'; ?>
+
+
+		<footer class="footer">
+			© 2002-2017 ГК ЗНАК
+		</footer>	
+		<div id="fullpage">
+			<div class="section started">
+				<video poster="assets/images/comp-1.jpg" id="bgvid-1" playsinline autoplay muted loop>
+                </video>
+				<div class="step step--main step--development-plus">
+					<div class="step__left-part ">
+						<h1>
+							<span>
+								<span>Группа компаний Знак - </span><br />проекты со <span>знаком</span> качества
+							</span>
+						</h1>
+						
+						<div class="about-companu-text">
+						<p>Группа компаний ЗНАК работает на рынке недвижимости с 2001 года. В отличии от lorem ipsum, текст рыба на русском языке наполнит любой макет непонятным смыслом и придаст неповторимый колорит</p>
+						<p>Равным образом начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки новых предложений. Повседневная практика показывает, что начало повседневной работы по формированию позиции в значительной степени обуславливает создание существенных финансовых и административных условий.</p>
+					
+				</div>
+					</div>
+
+
+
+
+				</div>				
+			</div>
+			<div class="section">
+				<video poster="assets/images/comp-2.jpg" id="bgvid-2" playsinline autoplay muted loop>
+                </video>
+				<div class="step step--main step--numbers">
+					<div class="step__left-part">
+						<h1>
+							<span>
+								<span>Партнеры</span><br /> ГК Знак
+							</span>
+						</h1>	
+					</div>
+					<div class="step__right-part">
+						 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+                              <br>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+					</div>
+				</div>				
+			</div>
+			<div class="section">
+                <video poster="assets/images/vacanсy.jpg" id="bgvid-2" playsinline autoplay muted loop></video>
+                </video>
+				<div class="step step--main step--development-1">
+					<div class="step__left-part">
+						<h1>
+							<span>
+								<span>Вакансии</span>
+							</span>
+						</h1>
+						<a class="step__button step__button--desktop" href="vacansies.php">Все вакансии</a>
+					</div>
+					<div class="step__right-part">
+						<table class="step__table step__table--numbers">
+							<tbody>
+								<tr>
+									<td class="title">Проектов в стадии реализации:</td>
+									<td>
+										<span><span class="odometer countTo" data-from="0" data-to="5"></span>проектов</span>
+									</td>
+								</tr>
+								<tr>
+									<td class="title">Потенциальный объем жилой и </br>коммерческой застройки: </td>
+									<td>
+										<span><span class="odometer countTo" data-from="0" data-to="1.8"></span>млн. кв.м.</span>
+									</td>
+								</tr>
+								<tr>
+									<td class="title">Объём инвестиций:</td>
+									<td>
+										<span><span class="odometer countTo" data-from="0" data-to="81.5"></span>млрд. руб.</span>
+									</td>
+								</tr>
+								<tr>
+									<td class="title">Регионы присутствия:</td>
+									<td>
+										<span><span class="odometer countTo" data-from="0" data-to="2"></span>региона</span>
+									</td>
+								</tr>								
+							</tbody>
+						</table>
+						<a class="step__button step__button--mobile" href="vacansies.php">Все проекты</a>
+					</div>
+				</div>				
+			</div>
+			
+				</div>				
+			</div>
+		</div>		
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/javascripts/jquery.easings.min.js"></script>
+		<script type="text/javascript" src="assets/javascripts/scrolloverflow.min.js"></script>
+		<script type="text/javascript" src="assets/libs/fullpage.js/jquery.fullpage.min.js"></script>
+		<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter41412469 = new Ya.Metrika({ id:41412469, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <!-- /Yandex.Metrika counter -->
+    </body>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-countto/1.2.0/jquery.countTo.min.js"></script>
+	<script type="text/javascript" src="assets/javascripts/frontend.js?ver=2"></script>
+
+</html>
---
+++ www/services.php
@@ -0,0 +1,157 @@
+<?include 'header.html'; ?>
+          <footer class="footer">
+               © 2002-2017 ГК ЗНАК
+          </footer> 
+          <div id="fullpage">
+               
+
+          <div class="section started">
+                <video poster="assets/images/serv-develop.jpg" id="bgvid-3" playsinline autoplay muted loop>
+                </video>
+                    <div class="step step--main step--development">
+                         <div class="step__left-part">
+                              <h1>
+                                                             <span>
+                                        <span>Девелопмент</span></span>
+                                   </span>
+                              </h1>
+                              
+                         </div>
+                         <div class="step__right-part">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+                              <br>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+                         </div>
+                    </div>                   
+               </div>
+
+
+               <div class="section">
+                    <video poster="assets/images/serv2.jpg" id="bgvid-2" playsinline autoplay muted loop>
+                </video>
+                    <div class="step step--main step--numbers">
+                         <div class="step__left-part">
+                              <h1><span>
+                                        <span>Управление </span><br>земельными<br>активами</span>
+                                   </span>
+         
+                              </h1>
+                              <a class="step__button step__button--desktop" href="land-list.php">Подробнее</a>      
+                         </div>
+                         <div class="step__right-part">
+                              <table class="step__table step__table--numbers">
+                                   <tbody>
+                                        <tr>
+                                             <td class="title">Количество участков:</td>
+                                             <td>
+                                                  <span><span class="odometer countTo" data-to="128">0</span></span>
+                                             </td>
+                                        </tr>
+                                        <tr>
+                                             <td class="title">Общая площадь участков:</td>
+                                             <td>
+                                                  <span><span class="odometer countTo" data-to="44">0</span>га.</span>
+                                             </td>
+                                        </tr>
+                                        <tr>
+                                             <td class="title">Средняя стоимость сотки:</td>
+                                             <td>
+                                                  <span><span class="odometer countTo" data-to="780">0</span>тыс. руб.</span>
+                                             </td>
+                                        </tr>
+                                        <tr>
+                                             <td class="title">Регионы присутствия:</td>
+                                             <td>
+                                                  <span><span class="odometer countTo" data-to="4">0</span>региона</span>
+                                             </td>
+                                        </tr>                                        
+                                   </tbody>
+                              </table>
+                         </div>
+                    </div>                   
+               </div>
+               <div class="section">
+                <video poster="assets/images/serv1.jpg" id="bgvid-3" playsinline autoplay muted loop>
+                </video>
+                    <div class="step step--main step--development">
+                         <div class="step__left-part">
+                              <h1>
+                                                             <span>
+                                        <span>Управление </span><br>объектами<br>недвижимости</span>
+                                   </span>
+                              </h1>
+                              
+                         </div>
+                         <div class="step__right-part">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+                              <br>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+                         </div>
+                    </div>                   
+               </div>
+               <div class="section">
+                <video poster="assets/images/serv3.jpg" id="bgvid-4" playsinline autoplay muted loop>
+                </video>
+                    <div class="step step--main step--development">
+                         <div class="step__left-part">
+                              <h1>
+                                   <span>
+                                        <span>Оценка</span><br>объектов<br>недвижимости</span>
+                                   </span>
+                              </h1>
+                             
+                         </div>
+                         <div class="step__right-part">
+ <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+                              <br>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sunt iusto nesciunt ex voluptatibus deserunt, commodi quod eius dolor. Obcaecati maxime autem odit eum provident necessitatibus, quibusdam veritatis vero eos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi veritatis quos delectus reprehenderit error quas ipsam ab dolor rem aspernatur eveniet cupiditate laboriosam eaque rerum totam mollitia, molestias dolore.</p>
+                         </div>
+                    </div>                   
+               </div>
+               <!-- <div class="section">
+                <video poster="/v4/assets/images/screen-5.jpg" id="bgvid-5" playsinline autoplay muted loop>
+                    <source src="/v4/assets/video/sclad.webm" type="video/webm">
+                    <source src="/v4/assets/video/sclad.mp4" type="video/mp4">
+                </video>
+                    <div class="step step--main step--development">
+                         <div class="step__left-part">
+                              <h1>
+                                   <span>
+                                        <span>Управление</span><br /> недвижимостью
+                                   </span>
+                              </h1>
+                                        
+                         </div>
+                         <div class="step__right-part">
+                              <table class="step__table step__table--numbers">
+                                   <tbody>
+                                        <tr>
+                                             <td class="title">Площадь объектов недвижимости:</td>
+                                             <td>
+                                                  <span><span class="odometer countTo" data-odometer countTo="91">0</span> тыс. кв.м.</span>
+                                             </td>
+                                        </tr>
+                                        <tr>
+                                             <td class="title">Общее количество арендаторов:</td>
+                                             <td>
+                                                  <span><span class="odometer countTo" data-odometer countTo="300">0</span> арендаторов</span>
+                                             </td>
+                                        </tr>                                   
+                                   </tbody>
+                              </table>
+                         </div>
+                    </div>                   
+               </div> -->
+
+          </div>         
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+          <script type="text/javascript" src="assets/javascripts/jquery.easings.min.js"></script>
+          <script type="text/javascript" src="assets/javascripts/scrolloverflow.min.js"></script>
+          <script type="text/javascript" src="assets/libs/fullpage.js/jquery.fullpage.min.js"></script>
+          <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter41412469 = new Ya.Metrika({ id:41412469, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <!-- /Yandex.Metrika counter -->
+    </body>
+     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+     <script src="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js"></script>
+     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-countto/1.2.0/jquery.countTo.min.js"></script> 
+     <script type="text/javascript" src="assets/javascripts/frontend.js?ver=2"></script>
+</html>
